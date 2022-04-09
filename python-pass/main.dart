faaa3621422b0c663bb6dbbb131996e6ded6927c
amjadmahmood97 <98470263+amjadmahmood97@users.noreply.github.com>
2022-04-10 02:33:46 +0300

    Update main.dart

--- python-pass/main.dart
+++ python-pass/main.dart
@@ -1,8 +1,10 @@
 /*
-Can't set const with time,
+Can't set const with time,because const A variable that is declared using the const keyword cannot be assigned any other value.
+Also, the variable is known as a compile-time constant, which in turn means that its value must be declared while compiling the program.
+
 And This is the correct solution
 void main() {
-  final hour = DateTime.now();
+  var hour = DateTime.now();
   print(hour);
 }
 */
